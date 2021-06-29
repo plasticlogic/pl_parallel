0727da328d36e54e7702ac26da446192ea0d1d89
plasticlg <lars.goerner@plasticlogic.com>
2021-06-29 15:23:09 +0200

    Added read fn + Changed read fn

--- include/ctrl/controller.h
+++ include/ctrl/controller.h
@@ -2,7 +2,7 @@
 /*
  * ctrl.h - controller interface
  *
- * Copyright (c) 2001-2003 PL Germany
+ * Copyright (c) 2021 PL Germany
  * 
  * Authors
  *      Lars Görner <lars.goerner@plasticlogic.com>
@@ -13,13 +13,17 @@
 #include <linux/kobject.h>
 #include <linux/gpio/consumer.h>
 
+#define PAR_CTRL_NAME "ctrl"
 #define HRDY_GPIO_ID    "hrdy"
 
 struct controller {
         struct kobject kobj;
         struct device *dev;
         int (*init)(struct controller *ctrl);
-        ssize_t (*write)(struct controller *ctrl, const char *buf, size_t len);
+        ssize_t (*read)(struct controller *ctrl, short addr, 
+                        short *buf, size_t len);
+        ssize_t (*write)(struct controller *ctrl, short addr, 
+                         const short *buf, size_t len);
         void (*destroy)(struct controller *ctrl);
         struct gpio_desc *hrdy_gpio;
 };
