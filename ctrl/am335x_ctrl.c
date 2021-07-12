942ccd90ebf8e6300c6f6c1cf34bdcb983877bd6
plasticlg <lars.goerner@plasticlogic.com>
2021-07-12 13:10:34 +0200

    Changed timings

--- ctrl/am335x_ctrl.c
+++ ctrl/am335x_ctrl.c
@@ -506,8 +506,8 @@ static void am335x_polarities_sysfs_unregister(struct am335x_ctrl *ctrl)
 ////////////////////////////////////////////////////////////////////////////////
 // Controller functions
 
-static const unsigned int init_hw_clk_freq = 200000000;
-static const int init_clk_div = 2;
+static const unsigned int init_hw_clk_freq = 300000000;
+static const int init_clk_div = 1;
 
 static struct am335x_lidd_timings init_timings = {
         .w_setup = 7,
